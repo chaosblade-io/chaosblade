69d075517174920127b101724058a3349cd3a3f6
jievince <thejackww@gmail.com>
2019-07-03 15:53:33 +0800

    feature: add travis CI service

--- exec/os/bin/burncpu/burncpu.go
+++ exec/os/bin/burncpu/burncpu.go
@@ -4,15 +4,17 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"path"
 	"runtime"
 	"strings"
 	"time"
-	"path"
 
-	"github.com/chaosblade-io/chaosblade/util"
-	"github.com/chaosblade-io/chaosblade/exec/os"
-	"github.com/chaosblade-io/chaosblade/exec"
 	"strconv"
+
+	"github.com/chaosblade-io/chaosblade/exec"
+	"github.com/chaosblade-io/chaosblade/exec/os"
+	"github.com/chaosblade-io/chaosblade/exec/os/bin"
+	"github.com/chaosblade-io/chaosblade/util"
 )
 
 var (
@@ -89,7 +91,7 @@ func runBurnCpu(ctx context.Context, cpuCount int, pidNeeded bool, processor str
 	response := exec.NewLocalChannel().Run(ctx, os.NohupCommand, args)
 	if !response.Success {
 		stopBurnCpu()
-		printErrAndExit(response.Err)
+		bin.PrintErrAndExit(response.Err)
 	}
 	if pidNeeded {
 		// parse pid
@@ -97,14 +99,14 @@ func runBurnCpu(ctx context.Context, cpuCount int, pidNeeded bool, processor str
 		pids, err := exec.GetPidsByProcessName(os.BurnCpuCommand, newCtx)
 		if err != nil {
 			stopBurnCpu()
-			printErrAndExit(fmt.Sprintf("bind cpu core failed, cannot get the burning program pid, %v", err))
+			bin.PrintErrAndExit(fmt.Sprintf("bind cpu core failed, cannot get the burning program pid, %v", err))
 		}
 		if len(pids) > 0 {
 			// return the first one
 			pid, err := strconv.Atoi(pids[0])
 			if err != nil {
 				stopBurnCpu()
-				printErrAndExit(fmt.Sprintf("bind cpu core failed, get pid failed, pids: %v, err: %v", pids, err))
+				bin.PrintErrAndExit(fmt.Sprintf("bind cpu core failed, get pid failed, pids: %v, err: %v", pids, err))
 			}
 			return pid
 		}
@@ -117,7 +119,7 @@ func bindBurnCpu(ctx context.Context, core string, pid int) {
 	response := exec.NewLocalChannel().Run(ctx, os.TasksetCommand, fmt.Sprintf("-cp %s %d", core, pid))
 	if !response.Success {
 		stopBurnCpu()
-		printErrAndExit(response.Err)
+		bin.PrintErrAndExit(response.Err)
 	}
 }
 
@@ -128,7 +130,7 @@ func checkBurnCpu(ctx context.Context) {
 	ctx = context.WithValue(ctx, exec.ProcessKey, os.NohupCommand)
 	pids, _ := exec.GetPidsByProcessName(os.BurnCpuCommand, ctx)
 	if pids == nil || len(pids) == 0 {
-		printErrAndExit(fmt.Sprintf("%s pid not found", os.BurnCpuCommand))
+		bin.PrintErrAndExit(fmt.Sprintf("%s pid not found", os.BurnCpuCommand))
 	}
 }
 
