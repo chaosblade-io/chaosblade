27180a4d33ad88c912788c5b45d4a0f9ed10ea1a
xcaspar <x.caspar@gmail.com>
2019-08-29 15:37:06 +0800

    feature: add path flag in disk fill to specify any directory

--- exec/os/bin/filldisk/filldisk.go
+++ exec/os/bin/filldisk/filldisk.go
@@ -13,11 +13,11 @@ import (
 )
 
 var fillDataFile = "chaos_filldisk.log.dat"
-var fillDiskMountPoint, fillDiskSize string
+var fillDiskSize, fillDiskDirectory string
 var fillDiskStart, fillDiskStop bool
 
 func main() {
-	flag.StringVar(&fillDiskMountPoint, "mount-point", "", "mount point of disk")
+	flag.StringVar(&fillDiskDirectory, "directory", "", "the directory where the disk is populated")
 	flag.StringVar(&fillDiskSize, "size", "", "fill size")
 	flag.BoolVar(&fillDiskStart, "start", false, "start fill or not")
 	flag.BoolVar(&fillDiskStop, "stop", false, "stop fill or not")
@@ -28,9 +28,9 @@ func main() {
 		bin.PrintErrAndExit("must specify start or stop operation")
 	}
 	if fillDiskStart {
-		startFill(fillDiskMountPoint, fillDiskSize)
+		startFill(fillDiskDirectory, fillDiskSize)
 	} else if fillDiskStop {
-		stopFill(fillDiskMountPoint)
+		stopFill(fillDiskDirectory)
 	} else {
 		bin.PrintErrAndExit("less --start or --stop flag")
 	}
@@ -38,18 +38,18 @@ func main() {
 
 var channel = exec.NewLocalChannel()
 
-func startFill(mountPoint, size string) {
+func startFill(directory, size string) {
 	ctx := context.Background()
-	if mountPoint == "" {
-		bin.PrintErrAndExit("mount-point flag is empty")
+	if directory == "" {
+		bin.PrintErrAndExit("--directory flag value is empty")
 	}
-	dataFile := path.Join(mountPoint, fillDataFile)
+	dataFile := path.Join(directory, fillDataFile)
 
 	// Some normal filesystems (ext4, xfs, btrfs and ocfs2) tack quick works
 	if exec.IsCommandAvailable("fallocate") {
 		response := channel.Run(ctx, "fallocate", fmt.Sprintf(`-l %sM %s`, size, dataFile))
 		if !response.Success {
-			stopFill(mountPoint)
+			stopFill(directory)
 			bin.PrintErrAndExit(response.Err)
 		}
 		bin.PrintOutputAndExit(response.Result.(string))
@@ -57,26 +57,26 @@ func startFill(mountPoint, size string) {
 
 	response := channel.Run(ctx, "dd", fmt.Sprintf(`if=/dev/zero of=%s bs=1b count=1 iflag=fullblock`, dataFile))
 	if !response.Success {
-		stopFill(mountPoint)
+		stopFill(directory)
 		bin.PrintErrAndExit(response.Err)
 	}
 	response = channel.Run(ctx, "nohup",
 		fmt.Sprintf(`dd if=/dev/zero of=%s bs=1M count=%s iflag=fullblock >/dev/null 2>&1 &`, dataFile, size))
 	if !response.Success {
-		stopFill(mountPoint)
+		stopFill(directory)
 		bin.PrintErrAndExit(response.Err)
 	}
 	bin.PrintOutputAndExit(response.Result.(string))
 }
 
-func stopFill(mountPoint string) {
+func stopFill(directory string) {
 	ctx := context.Background()
 	pids, _ := exec.GetPidsByProcessName(fillDataFile, ctx)
 
 	if pids != nil || len(pids) >= 0 {
 		channel.Run(ctx, "kill", fmt.Sprintf("-9 %s", strings.Join(pids, " ")))
 	}
-	fileName := path.Join(mountPoint, fillDataFile)
+	fileName := path.Join(directory, fillDataFile)
 	if util.IsExist(fileName) {
 		response := channel.Run(ctx, "rm", fmt.Sprintf(`-rf %s`, fileName))
 		if !response.Success {
