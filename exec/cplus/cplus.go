d7ae88470d31436dc369b1ed3ce9c89ec0f5ab32
xcaspar <changjun.xcj@alibaba-inc.com>
2025-09-09 16:26:11 +0800

    Bugfix: Fix cplus failure scenario execution
    Signed-off-by: xcaspar <changjun.xcj@alibaba-inc.com>

--- exec/cplus/cplus.go
+++ exec/cplus/cplus.go
@@ -19,10 +19,11 @@ package cplus
 import (
 	"context"
 	"fmt"
-	"github.com/chaosblade-io/chaosblade-spec-go/log"
 	"path"
 	"strings"
 	"time"
+
+	"github.com/chaosblade-io/chaosblade-spec-go/log"
 
 	"github.com/chaosblade-io/chaosblade-spec-go/channel"
 	"github.com/chaosblade-io/chaosblade-spec-go/spec"
@@ -35,7 +36,7 @@ const RemoveAction = "remove"
 var cplusBinPath = path.Join(util.GetLibHome(), "cplus", ApplicationName)
 var scriptDefaultPath = path.Join(util.GetLibHome(), "cplus", "script")
 
-// 启动 spring boot application，需要校验程序是否已启动
+// 启动 cplus application，需要校验程序是否已启动
 func Prepare(ctx context.Context, port, ip string) *spec.Response {
 
 	response := preCheck(ctx, port)
@@ -50,11 +51,11 @@ func Prepare(ctx context.Context, port, ip string) *spec.Response {
 }
 
 func preCheck(ctx context.Context, port string) *spec.Response {
-	// check spring boot application
+	// check cplus application
 	if processExists(port) {
 		return spec.ReturnSuccess("the server proxy has been started")
 	}
-	// check chaosblade-exec-cplus.jar file exists or not
+	// check chaosblade-exec-cplus file exists or not
 	if !util.IsExist(cplusBinPath) {
 		log.Errorf(ctx, spec.ChaosbladeFileNotFound.Sprintf(cplusBinPath))
 		return spec.ResponseFailWithFlags(spec.ChaosbladeFileNotFound, cplusBinPath)
@@ -67,8 +68,8 @@ func preCheck(ctx context.Context, port string) *spec.Response {
 	// check the port has been used or not
 	portInUse := util.CheckPortInUse(port)
 	if portInUse {
-		log.Errorf(ctx, spec.ParameterInvalid.Sprintf("port", port, "the port has been used"))
-		return spec.ResponseFailWithFlags(spec.ParameterInvalid, port, "the port has been used")
+		log.Errorf(ctx, spec.ParameterInvalid.Sprintf("port", port, "the port has been used", ""))
+		return spec.ResponseFailWithFlags(spec.ParameterInvalid, "port", port, "the port has been used", "")
 	}
 	return spec.ReturnSuccess("success")
 }
@@ -100,7 +101,7 @@ func postCheck(ctx context.Context, port string) *spec.Response {
 	return spec.ReturnSuccess(result)
 }
 
-// 停止 spring boot application
+// 停止 cplus application
 func Revoke(ctx context.Context, port string) *spec.Response {
 	// check process
 	if !processExists(port) {
@@ -109,7 +110,7 @@ func Revoke(ctx context.Context, port string) *spec.Response {
 	// Get http://127.0.0.1:xxx/remove: EOF, doesn't to check the result
 	util.Curl(ctx, getProxyServiceUrl(port, RemoveAction))
 	time.Sleep(time.Second)
-	ctx = context.WithValue(ctx, channel.ExcludeProcessKey, "blade")
+	// ctx = context.WithValue(ctx, channel.ExcludeProcessKey, "blade")
 	pids, err := channel.NewLocalChannel().GetPidsByProcessName(ApplicationName, ctx)
 	if err != nil {
 		log.Errorf(ctx, spec.ProcessIdByNameFailed.Sprintf(ApplicationName, err))
